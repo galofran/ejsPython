e83f09a7a0d138e481ce95f1df5e34faa0074c97
galo <galofran97@gmail.com>
2024-02-26 19:26:22 +0100

    Primer ejecicio

---
+++ PrimeroDiezRegistros.py
@@ -0,0 +1,18 @@
+import pyodbc
+
+try:
+    connection = pyodbc.connect('DRIVER={SQL Server};SERVER=AL12059\SQLEXPRESS;DATABASE=walmart;UID=usuario1;PWD=usuario1')
+    #En mi caso mi servidor es " . " y mi base de datos se llama "Pruebaa", reemplazas por los valores que tengas
+    print("Conexion Exitosa") #Ponemos este mensaje para que nos confirme la conexion
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM Walmart_sales ORDER BY Temperature OFFSET 0 ROWS FETCH NEXT 10 ROWS ONLY;")
+    #SELECT TOP 10 * FROM Walmart_sales;
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    
+except Exception as ex:
+    print(ex)
+finally:
+    if connection:
+        connection.close() #cerramos la conexion para ahorrar recursos
